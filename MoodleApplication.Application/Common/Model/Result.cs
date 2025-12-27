efa9a9ae588f53c1ee5caa6e206674f448902688
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-12-28 00:26:13 +0100

    Add Application result

---
+++ MoodleApplication.Application/Common/Model/Result.cs
@@ -0,0 +1,53 @@
+﻿
+using MoodleApplication.Domain.Common.Validation;
+using MoodleApplication.Domain.Enumumerations.Validations;
+
+namespace MoodleApplication.Application.Common.Model
+{
+    public class Result<TVaule> where TVaule : class
+    {
+        private List<ValidationResultItem> _info = new();
+        private List<ValidationResultItem> _warnings = new();
+        private List<ValidationResultItem> _errors = new();
+        public TVaule? Value { get; set; }
+        public Guid RequestId { get; set; }
+        public bool IsAuthorized { get; set; } = true;
+
+        public IReadOnlyList<ValidationResultItem> Infos
+        {
+            get => _info.AsReadOnly();
+            init => _info.AddRange(value);
+        }
+        public IReadOnlyList<ValidationResultItem> Errors
+        {
+            get => _errors.AsReadOnly();
+            init => _errors.AddRange(value);
+        }
+        public IReadOnlyList<ValidationResultItem> Warnings
+        {
+            get => _warnings.AsReadOnly();
+            init => _warnings.AddRange(value);
+        }
+        public bool HasError => Errors.Any(validationResult => validationResult.ValidationSeverity == ValidationSeverity.Error);
+        public bool HasInfo => Infos.Any(validationResult => validationResult.ValidationSeverity == ValidationSeverity.Info);
+        public bool HasWarning => Warnings.Any(validationResult => validationResult.ValidationSeverity == ValidationSeverity.Warning);
+
+        public void SetResult(TVaule? value)
+        {
+            Value = value;
+        }
+        public void SetValidationResult(ValidationResult validationResult)
+        {
+            _errors?.AddRange(validationResult.ValidationItems.Where(x => x.ValidationSeverity == ValidationSeverity.Error).Select(x => ValidationResultItem.FromValidationItem(x)));
+
+        }
+
+        public void SetUnaAuthorizedResult() { 
+
+
+            Value = null;
+            IsAuthorized = false;
+        }
+
+    }
+}
---
+++ MoodleApplication.Application/Common/Model/ValidationResultItem.cs
@@ -0,0 +1,26 @@
+﻿
+
+using MoodleApplication.Domain.Common.Validation;
+using MoodleApplication.Domain.Enumumerations.Validations;
+
+namespace MoodleApplication.Application.Common.Model
+{
+    public class ValidationResultItem
+    {
+        public ValidationSeverity ValidationSeverity { get; init; }
+        public ValidationType ValidationType { get; init; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+
+        public static ValidationResultItem FromValidationItem(ValidationItem item)
+        {
+            return new ValidationResultItem
+            {
+                ValidationSeverity = item.ValidationSeverity,
+                ValidationType = item.ValidationType,
+                Code = item.Code,
+                Message = item.Message
+            };
+        }
+    }
+}
