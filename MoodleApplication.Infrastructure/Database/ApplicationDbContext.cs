11cd6340b099217a4e6f8ac2ea4bbf3f01925a0b
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-12-31 15:03:25 +0100

    Add Infrastructure application db context

---
+++ MoodleApplication.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoodleApplication.Domain.Entities.Chats;
+using MoodleApplication.Domain.Entities.Courses;
+using MoodleApplication.Domain.Entities.Users;
+
+namespace MoodleApplication.Infrastructure.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Course> Courses { get; set; } = null!;
+        public DbSet<CourseStudent> CourseStudents { get; set; } = null!;
+        public DbSet<ChatRoom> ChatRooms { get; set; } = null!;
+        public DbSet<Message> Messages { get; set; } = null!;
+        public DbSet<Announcement> Announcements { get; set; } = null!;
+        public DbSet<Material> Materials { get; set; } = null!;
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            modelBuilder.HasDefaultSchema("public");
+        }
+    }
+}
