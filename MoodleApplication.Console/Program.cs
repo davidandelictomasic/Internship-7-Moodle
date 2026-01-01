f20b89fe2ff4708380e3b73687f2cd11ec52ba7e
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-01-01 16:29:35 +0100

    Add Console appsettings file

--- MoodleApplication.Console/Program.cs
+++ MoodleApplication.Console/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
 Console.WriteLine("Hello, World!");
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+
