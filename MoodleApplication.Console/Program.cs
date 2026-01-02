6d8f3dc73d5bbb4bf5a197b612e7ba3cdb7e0f8b
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-01-02 10:17:40 +0100

    Add Console menu manager and options

--- MoodleApplication.Console/Program.cs
+++ MoodleApplication.Console/Program.cs
@@ -1,13 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MoodleApplication.Application.Users.User;
+using MoodleApplication.Infrastructure.Database;
 
 Console.WriteLine("Hello, World!");
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .Build();
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<CreateUserRequestHandler>();
+builder.Build();
 
 
---
+++ MoodleApplication.Console/Views/MenuOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace MoodleApplication.Console.Views
+{
+    public class MenuOptions
+    {
+        private readonly Dictionary<string, (string Description, Func<Task<bool>> Action)> _options;
+
+        public MenuOptions()
+        {
+            _options = [];
+        }
+
+        public MenuOptions AddOption(string key, string description, Func<Task<bool>> action)
+        {
+            _options.Add(key, (description, action));
+            return this;
+        }
+
+        public Dictionary<string, (string Description, Func<Task<bool>> Action)> Build()
+        {
+            return _options;
+        }
+    }
+}
